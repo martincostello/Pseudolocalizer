5959199c75ccfaee488b43db256dc646627bac3f
martincostello <martin@martincostello.com>
2018-08-25 12:48:00 +0100

    Add support for XLIFF 2.0
    Add support for XLIFF version 2.0.
    Only update the state attribute, do not add it.

--- PseudoLocalizer.Core.Tests/XlfProcessorTests.cs
+++ PseudoLocalizer.Core.Tests/XlfProcessorTests.cs
@@ -6,7 +6,8 @@
     [TestFixture]
     public class XlfProcessorTests
     {
-        private const string Test1FileName = "Test1.xlf";
+        private const string Xlf12FileName = "1.2.xlf";
+        private const string Xlf20FileName = "2.0.xlf";
         private const string OutputFileName = "out.xlf";
 
         [SetUp]
@@ -16,22 +17,26 @@
         }
 
         [Test]
-        public void ShouldLeaveTheXmlUntouchedWhenUsingAnIdentityTransformation()
+        [TestCase(Xlf12FileName)]
+        [TestCase(Xlf20FileName)]
+        public void ShouldLeaveTheXmlUntouchedWhenUsingAnIdentityTransformation(string inputFileName)
         {
-            using (var inputStream = new FileStream(Test1FileName, FileMode.Open, FileAccess.Read))
+            using (var inputStream = new FileStream(inputFileName, FileMode.Open, FileAccess.Read))
             using (var outputStream = new FileStream(OutputFileName, FileMode.Create, FileAccess.Write))
             {
                 var processor = new XlfProcessor();
                 processor.Transform(inputStream, outputStream);
             }
 
-            Assert.That(File.ReadAllBytes(Test1FileName), Is.EqualTo(File.ReadAllBytes(OutputFileName)), "the output file is identical to the input file.");
+            Assert.That(File.ReadAllBytes(inputFileName), Is.EqualTo(File.ReadAllBytes(OutputFileName)), "the output file is identical to the input file.");
         }
 
         [Test]
-        public void ShouldReverseStringsButLeaveTheCommentsUntouchedWhenTransformingWithAStringReverseTransformation()
+        [TestCase(Xlf12FileName)]
+        [TestCase(Xlf20FileName)]
+        public void ShouldReverseStringsButLeaveTheCommentsUntouchedWhenTransformingWithAStringReverseTransformation(string inputFileName)
         {
-            using (var inputStream = new FileStream(Test1FileName, FileMode.Open, FileAccess.Read))
+            using (var inputStream = new FileStream(inputFileName, FileMode.Open, FileAccess.Read))
             using (var outputStream = new FileStream(OutputFileName, FileMode.Create, FileAccess.Write))
             {
                 var processor = new XlfProcessor();
@@ -39,20 +44,20 @@
                 processor.Transform(inputStream, outputStream);
             }
 
-            var original = File.ReadAllText(Test1FileName);
+            var original = File.ReadAllText(Xlf12FileName);
             var transformed = File.ReadAllText(OutputFileName);
             Assert.That(original.Contains("<source>Something</source>"));
             Assert.That(!original.Contains("<target state=\"translated\">gnihtemoS</target>"));
             Assert.That(transformed.Contains("<target state=\"translated\">gnihtemoS</target>"));
             Assert.That(!transformed.Contains("<target state=\"translated\">Something</target>"));
-            Assert.That(original.Contains("<note>Blah</note>"));
-            Assert.That(transformed.Contains("<note>Blah</note>"));
         }
 
         [Test]
-        public void ShouldAddFunnyAccentsWhenTransformingWithTheAccenterTransformation()
+        [TestCase(Xlf12FileName)]
+        [TestCase(Xlf20FileName)]
+        public void ShouldAddFunnyAccentsWhenTransformingWithTheAccenterTransformation(string inputFileName)
         {
-            using (var inputStream = new FileStream(Test1FileName, FileMode.Open, FileAccess.Read))
+            using (var inputStream = new FileStream(inputFileName, FileMode.Open, FileAccess.Read))
             using (var outputStream = new FileStream(OutputFileName, FileMode.Create, FileAccess.Write))
             {
                 var processor = new XlfProcessor();
@@ -61,15 +66,16 @@
             }
 
             var transformed = File.ReadAllText(OutputFileName);
-            Assert.That(!transformed.Contains("<target state=\"translated\">Dude</target>"));
-            Assert.That(transformed.Contains("<note>Foo</note>"));
-            Assert.That(transformed.Contains("<target state=\"translated\">\u00d0\u00fb\u00f0\u00e9</target>"));
+            Assert.That(!transformed.Contains("<target>Dude</target>"));
+            Assert.That(transformed.Contains("<target>\u00d0\u00fb\u00f0\u00e9</target>"));
         }
 
         [Test]
-        public void ShouldApplyMultipleTransformations()
+        [TestCase(Xlf12FileName)]
+        [TestCase(Xlf20FileName)]
+        public void ShouldApplyMultipleTransformationsXlf(string inputFileName)
         {
-            using (var inputStream = new FileStream(Test1FileName, FileMode.Open, FileAccess.Read))
+            using (var inputStream = new FileStream(inputFileName, FileMode.Open, FileAccess.Read))
             using (var outputStream = new FileStream(OutputFileName, FileMode.Create, FileAccess.Write))
             {
                 var processor = new XlfProcessor();
@@ -79,14 +85,14 @@
                 processor.Transform(inputStream, outputStream);
             }
 
-            var original = File.ReadAllText(Test1FileName);
+            var original = File.ReadAllText(Xlf12FileName);
             var transformed = File.ReadAllText(OutputFileName);
             Assert.That(original.Contains("<source>Dude</source>"));
             Assert.That(original.Contains("<source>Whatever</source>"));
             Assert.That(original.Contains("<source>Something</source>"));
             Assert.That(original.Contains("<source>Anything</source>"));
-            Assert.That(transformed.Contains("<target state=\"translated\">[Dude1]2</target>"));
-            Assert.That(transformed.Contains("<target state=\"translated\">[Whatever1]2</target>"));
+            Assert.That(transformed.Contains("<target>[Dude1]2</target>"));
+            Assert.That(transformed.Contains("<target>[Whatever1]2</target>"));
             Assert.That(transformed.Contains("<target state=\"translated\">[Something1]2</target>"));
             Assert.That(transformed.Contains("<target state=\"translated\">[Anything1]2</target>"));
         }
--- PseudoLocalizer.Core/XlfProcessor.cs
+++ PseudoLocalizer.Core/XlfProcessor.cs
@@ -1,5 +1,8 @@
 ﻿namespace PseudoLocalizer.Core
 {
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
     using System.Xml;
 
     /// <summary>
@@ -7,6 +10,8 @@
     /// </summary>
     public sealed class XlfProcessor : XmlProcessor
     {
+        private readonly IDictionary<XmlDocument, Version> _versionCache = new ConcurrentDictionary<XmlDocument, Version>();
+
         /// <inheritdoc />
         protected override XmlNodeList SelectNodes(XmlDocument document, XmlNamespaceManager nsmgr)
         {
@@ -15,11 +20,65 @@
                 nsmgr.AddNamespace("x", document.DocumentElement.NamespaceURI);
             }
 
-            return document.SelectNodes("/x:xliff/x:file/x:body/x:trans-unit", nsmgr);
+            var version = GetXliffVersion(document);
+
+            string xpath;
+
+            if (version.Major == 1)
+            {
+                xpath = "/x:xliff/x:file/x:body/x:trans-unit";
+            }
+            else if (version.Major == 2)
+            {
+                xpath = "/x:xliff/x:file/x:unit";
+            }
+            else
+            {
+                throw new NotSupportedException($"XLIFF version {version} is not supported.");
+            }
+
+            return document.SelectNodes(xpath, nsmgr);
         }
 
         /// <inheritdoc />
         protected override void Visit(XmlNode node, XmlNamespaceManager nsmgr)
+        {
+            var version = GetXliffVersion(node.OwnerDocument);
+
+            if (version.Major == 1)
+            {
+                Visit10(node, nsmgr);
+            }
+            else if (version.Major == 2)
+            {
+                Visit20(node, nsmgr);
+            }
+            else
+            {
+                throw new NotSupportedException($"XLIFF version {version} is not supported.");
+            }
+        }
+
+        private Version GetXliffVersion(XmlDocument document)
+        {
+            if (_versionCache.TryGetValue(document, out Version version))
+            {
+                return version;
+            }
+
+            var versionString = document.DocumentElement.Attributes["version"]?.Value ?? string.Empty;
+
+            if (!Version.TryParse(versionString, out version))
+            {
+                version = new Version(1, 0);
+            }
+
+            _versionCache[document] = version;
+
+            return version;
+        }
+
+        private void Visit10(XmlNode node, XmlNamespaceManager nsmgr)
         {
             var document = node.OwnerDocument;
             var rootNamespace = document.DocumentElement.NamespaceURI;
@@ -42,13 +101,44 @@
 
                     var state = target.Attributes["state"];
 
-                    if (state == null)
+                    if (state != null)
                     {
-                        state = document.CreateAttribute("state");
-                        target.Attributes.Append(state);
+                        state.Value = "translated";
                     }
 
-                    state.Value = "translated";
+                    target.InnerText = transformed;
+                }
+            }
+        }
+
+        private void Visit20(XmlNode node, XmlNamespaceManager nsmgr)
+        {
+            var document = node.OwnerDocument;
+            var rootNamespace = document.DocumentElement.NamespaceURI;
+            var source = node.SelectSingleNode("x:segment/x:source", nsmgr);
+
+            if (source != null && source.NodeType == XmlNodeType.Element)
+            {
+                var original = source.InnerText;
+                var transformed = Transform(original);
+
+                if (transformed != original)
+                {
+                    var target = node.SelectSingleNode("x:segment/x:target", nsmgr);
+
+                    if (target == null)
+                    {
+                        target = document.CreateElement("target", rootNamespace);
+                        source.ParentNode.AppendChild(target);
+                    }
+
+                    var state = target.Attributes["state"];
+
+                    if (state != null)
+                    {
+                        state.Value = "translated";
+                    }
+
                     target.InnerText = transformed;
                 }
             }
