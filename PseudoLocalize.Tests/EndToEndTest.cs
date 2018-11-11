a6a268ba1d2917a95c56044d3a976d81c2108f28
martincostello <martin@martincostello.com>
2018-11-11 18:54:34 +0000

    Add end-to-end test for CLI
    Add an end-to-end test for the CLI application.

---
+++ PseudoLocalize.Tests/EndToEndTest.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PseudoLocalizer
+{
+    [TestFixture]
+    public class EndToEndTest
+    {
+        [Test]
+        public async Task ShouldLeaveTheXmlUntouchedWhenAlreadyTransformed()
+        {
+            // Arrange
+            string fileName = Path.GetFullPath("localized.xlf");
+            byte[] original = await File.ReadAllBytesAsync(fileName);
+
+            // Act
+            Program.Main(new[] { fileName, "--overwrite", "--force" });
+
+            // Assert
+            Assert.That(File.ReadAllBytes(fileName), Is.EqualTo(original), "The input file has changed.");
+        }
+    }
+}
