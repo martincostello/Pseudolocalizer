ac5977d95aab2bba96b3e790d0f9978af9d1c0f8
martincostello <martin@martincostello.com>
2018-11-11 16:18:40 +0000

    Preserve BOM in XML files
    Preserve any BOM in XML files that are processed.

--- PseudoLocalizer.Core/XmlProcessor.cs
+++ PseudoLocalizer.Core/XmlProcessor.cs
@@ -1,6 +1,7 @@
 ﻿namespace PseudoLocalizer.Core
 {
     using System.IO;
+    using System.Text;
     using System.Xml;
 
     /// <summary>
@@ -23,7 +24,14 @@
 
             OnProcessed(document, nsmgr, modified);
 
-            using (var xmlWriter = XmlWriter.Create(outputStream))
+            bool encoderShouldEmitUTF8Identifier = ShouldPreserveBOM(inputStream);
+
+            var settings = new XmlWriterSettings()
+            {
+                Encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier),
+            };
+
+            using (var xmlWriter = XmlWriter.Create(outputStream, settings))
             {
                 document.WriteTo(xmlWriter);
             }
@@ -74,5 +82,42 @@
         /// <see langword="true"/> if the node was modified; otherwise <see langword="false"/>.
         /// </returns>
         protected abstract bool Visit(XmlNode node, XmlNamespaceManager nsmgr);
+
+        private bool ShouldPreserveBOM(Stream inputStream)
+        {
+            bool preserveBOM = false;
+
+            if (inputStream.CanSeek)
+            {
+                long offset = inputStream.Position;
+                inputStream.Seek(0, SeekOrigin.Begin);
+
+                byte[] preamble = Encoding.UTF8.GetPreamble();
+
+                if (preamble.Length > 0)
+                {
+                    bool equal = true;
+
+                    for (int i = 0; i < preamble.Length && equal; i++)
+                    {
+                        int j = inputStream.ReadByte();
+
+                        if (j == -1)
+                        {
+                            equal = false;
+                            break;
+                        }
+
+                        equal &= preamble[i] == (byte)j;
+                    }
+
+                    preserveBOM = equal;
+                }
+
+                inputStream.Seek(offset, SeekOrigin.Begin);
+            }
+
+            return preserveBOM;
+        }
     }
 }
