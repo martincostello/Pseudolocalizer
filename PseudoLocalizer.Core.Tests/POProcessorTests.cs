e665a89227c7cb15d3652f5b74a8c10c666d86a1
Rich Tebb <rich@tebb.me.uk>
2019-09-19 10:47:58 +0100

    Add support for Gettext (PO) resource file format

---
+++ PseudoLocalizer.Core.Tests/POProcessorTests.cs
@@ -0,0 +1,84 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace PseudoLocalizer.Core.Tests
+{
+    [TestFixture]
+    public class POProcessorTests
+    {
+        private const string Test1FileName = "Test1.po";
+        private const string OutputFileName = "out.po";
+        
+        [SetUp]
+        public void SetUp()
+        {
+            DeleteOutputFile();
+        }
+
+        [Test]
+        public void ShouldLeaveTheFileUntouchedWhenUsingAnIdentityTransformation()
+        {
+            using (var inputStream = new FileStream(Test1FileName, FileMode.Open, FileAccess.Read))
+            using (var outputStream = new FileStream(OutputFileName, FileMode.Create, FileAccess.Write))
+            {
+                var processor = new POProcessor("en");
+                processor.Transform(inputStream, outputStream);
+            }
+
+            FileAssert.AreEqual(Test1FileName, OutputFileName);
+        }
+
+        [Test]
+        public void ShouldReverseStringsButLeaveTheCommentsUntouchedWhenTransformingWithAStringReverseTransformation()
+        {
+            using (var inputStream = new FileStream(Test1FileName, FileMode.Open, FileAccess.Read))
+            using (var outputStream = new FileStream(OutputFileName, FileMode.Create, FileAccess.Write))
+            {
+                var processor = new POProcessor("qps-Ploc");
+                processor.TransformString += (s, e) => { e.Value = Mirror.Instance.Transform(e.Value); };
+                processor.Transform(inputStream, outputStream);
+            }
+
+            FileAssert.AreEqual("Mirror.po", OutputFileName);
+        }
+
+        [Test]
+        public void ShouldAddFunnyAccentsWhenTransformingWithTheAccenterTransformation()
+        {
+            using (var inputStream = new FileStream(Test1FileName, FileMode.Open, FileAccess.Read))
+            using (var outputStream = new FileStream(OutputFileName, FileMode.Create, FileAccess.Write))
+            {
+                var processor = new POProcessor("qps-Ploc");
+                processor.TransformString += (s, e) => { e.Value = Accents.Instance.Transform(e.Value); };
+                processor.Transform(inputStream, outputStream);
+            }
+
+            FileAssert.AreEqual("Accents.po", OutputFileName);
+        }
+
+        [Test]
+        public void ShouldApplyMultipleTransformations()
+        {
+            using (var inputStream = new FileStream(Test1FileName, FileMode.Open, FileAccess.Read))
+            using (var outputStream = new FileStream(OutputFileName, FileMode.Create, FileAccess.Write))
+            {
+                var processor = new POProcessor("qps-Ploc");
+                processor.TransformString += (s, e) => { e.Value = e.Value + "1"; };
+                processor.TransformString += (s, e) => { e.Value = Brackets.Instance.Transform(e.Value); };
+                processor.TransformString += (s, e) => { e.Value = e.Value + "2"; };
+                processor.Transform(inputStream, outputStream);
+            }
+
+            FileAssert.AreEqual("Multiple.po", OutputFileName);
+        }
+
+        private static void DeleteOutputFile()
+        {
+            if (File.Exists(OutputFileName))
+            {
+                File.Delete(OutputFileName);
+                Assert.That(!File.Exists(OutputFileName));
+            }
+        }
+    }
+}
---
+++ PseudoLocalizer.Core/POProcessor.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Karambolo.PO;
+
+namespace PseudoLocalizer.Core
+{
+    /// <summary>
+    /// Applies transforms to string values in PO resource files.
+    /// </summary>
+    public class POProcessor : Processor
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="POProcessor"/> class.
+        /// </summary>
+        public POProcessor()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="POProcessor"/> class.
+        /// </summary>
+        /// <param name="culture">The output culture.</param>
+        public POProcessor(string culture)
+        {
+            Culture = culture ?? throw new ArgumentNullException(nameof(culture));;
+        }
+
+        /// <summary>
+        /// Gets the culture code associated with the processor.
+        /// </summary>
+        public string Culture { get; }
+
+        /// <inheritdoc />
+        public override void Transform(Stream inputStream, Stream outputStream)
+        {
+            var parser = new POParser(new POParserSettings());
+
+            var result = parser.Parse(inputStream);
+
+            if (result.Success)
+            {
+                var inputCatalog = result.Catalog;
+                var outputCatalog = ProcessCatalog(inputCatalog);
+
+                var generator = new POGenerator(new POGeneratorSettings());
+
+                generator.Generate(outputStream, outputCatalog);
+            }
+            else
+            {
+                var diagnostics = result.Diagnostics;
+                throw new Exception(diagnostics.ToString());
+            }
+        }
+
+        private POCatalog ProcessCatalog(POCatalog inputCatalog)
+        {
+            var outputCatalog = new POCatalog
+                {
+                    HeaderComments = inputCatalog.HeaderComments,
+                    Headers = inputCatalog.Headers,
+                    Encoding = inputCatalog.Encoding,
+                    Language = Culture,
+                    PluralFormCount = inputCatalog.PluralFormCount,
+                    PluralFormSelector = inputCatalog.PluralFormSelector
+                };
+
+            // Header comments etc.
+
+            // Entries
+            foreach (var entry in inputCatalog.Values)
+            {
+                if (entry is POSingularEntry singularEntry)
+                {
+                    outputCatalog.Add(TransformSingularEntry(singularEntry));
+                }
+                else if (entry is POPluralEntry plural)
+                {
+                    outputCatalog.Add(TransformPluralEntry(plural));
+                }
+                else
+                {
+                    throw new NotSupportedException("Unsupported PO entry type " + entry.GetType());
+                }
+            }
+
+            return outputCatalog;
+        }
+
+        private POPluralEntry TransformPluralEntry(POPluralEntry plural)
+        {
+            var entry = new POPluralEntry(plural.Key) {Comments = plural.Comments};
+            foreach (var source in plural)
+            {
+                entry.Add(Transform(source));
+            }
+
+            return entry;
+        }
+
+        private POSingularEntry TransformSingularEntry(POSingularEntry singular)
+        {
+            var entry = new POSingularEntry(singular.Key)
+            {
+                Translation = Transform(singular.Translation),
+                Comments = singular.Comments
+            };
+            return entry;
+        }
+    }
+}
