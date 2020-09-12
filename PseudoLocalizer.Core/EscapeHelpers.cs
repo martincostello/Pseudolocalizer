daf8ecfc256360342f38be1bb2f95068d79b8aa6
martincostello <martin@martincostello.com>
2020-09-12 16:08:55 +0100

    Do not mirror HTML tags
    Do not mirror HTML tags, just the text around and inside them.

--- PseudoLocalizer.Core/EscapeHelpers.cs
+++ PseudoLocalizer.Core/EscapeHelpers.cs
@@ -4,6 +4,15 @@ namespace PseudoLocalizer.Core
 {
     internal static class EscapeHelpers
     {
+        internal enum TextType
+        {
+            None = 0,
+            Format,
+            HtmlSelfClosing,
+            HtmlStart,
+            HtmlEnd,
+        }
+
         internal static bool MayNeedEscaping(string value)
         {
             return (value.Contains('{') && value.Contains('}')) ||
@@ -11,6 +20,9 @@ namespace PseudoLocalizer.Core
         }
 
         internal static bool ShouldTransform(char[] array, char ch, ref int i)
+            => ShouldTransform(array, ch, ref i, out var _);
+
+        internal static bool ShouldTransform(char[] array, char ch, ref int i, out TextType textType)
         {
             // Are we at the start of a potential placeholder (e.g. "{?...}")
             if (ch == '{' && i < array.Length - 2)
@@ -33,6 +45,7 @@ namespace PseudoLocalizer.Core
                 if (array[j] == '}')
                 {
                     i = j;
+                    textType = TextType.Format;
                     return false;
                 }
             }
@@ -41,13 +54,25 @@ namespace PseudoLocalizer.Core
                 // Are we at the start of a potential HTML tag (e.g. "<a/>")
                 int j = i;
 
+                textType = TextType.HtmlSelfClosing;
+
                 char next = array[i + 1];
 
                 if ((next >= 'a' && next <= 'z') || (next >= 'A' && next <= 'Z') || next == '/')
                 {
+                    if (next == '/')
+                    {
+                        textType = TextType.HtmlEnd;
+                    }
+
                     // Consume all of the tag
                     while (j < array.Length - 1 && array[++j] != '>')
                     {
+                    }
+
+                    if (array[j - 1] != '/' && textType != TextType.HtmlEnd)
+                    {
+                        textType = TextType.HtmlStart;
                     }
 
                     if (array[j] == '>')
@@ -58,6 +83,7 @@ namespace PseudoLocalizer.Core
                 }
             }
 
+            textType = TextType.None;
             return true;
         }
     }
--- PseudoLocalizer.Core/Mirror.cs
+++ PseudoLocalizer.Core/Mirror.cs
@@ -23,31 +23,98 @@
             {
                 char[] src = value.ToArray();
 
+                var startTag = new StringBuilder();
                 var builder = new StringBuilder(value.Length);
                 var current = new StringBuilder(value.Length);
+
+                bool insideTag = false;
 
                 for (int i = 0; i < value.Length; i++)
                 {
                     char ch = value[i];
                     int indexBefore = i;
 
-                    if (EscapeHelpers.ShouldTransform(src, ch, ref i))
+                    if (EscapeHelpers.ShouldTransform(src, ch, ref i, out var textType))
                     {
-                        current.Insert(0, FlipIfSpecial(ch));
+                        if (insideTag)
+                        {
+                            current.Insert(0, FlipIfSpecial(ch));
+                        }
+                        else
+                        {
+                            builder.Insert(0, FlipIfSpecial(ch));
+                        }
                     }
                     else
                     {
                         // Transformation should be skipped due to formatting placeholder or HTML
-                        // Add the skipped range
-                        for (int j = indexBefore; j < i + 1; j++)
+                        if (textType == EscapeHelpers.TextType.Format)
                         {
-                            builder.Append(value[j]);
-                        }
+                            // Add the skipped range
+                            if (insideTag)
+                            {
+                                for (int j = indexBefore, k = 0; j < i + 1; j++, k++)
+                                {
+                                    current.Insert(k, value[j]);
+                                }
+                            }
+                            else
+                            {
+                                for (int j = indexBefore; j < i + 1; j++)
+                                {
+                                    current.Append(value[j]);
+                                }
+                            }
 
-                        if (current.Length > 0)
+                            if (current.Length > 0 && !insideTag)
+                            {
+                                builder.Insert(0, current.ToString());
+                                current.Clear();
+                            }
+                        }
+                        else if (textType == EscapeHelpers.TextType.HtmlSelfClosing)
                         {
-                            builder.Append(current.ToString());
-                            current.Clear();
+                            // Add the skipped range
+                            for (int j = indexBefore, k = 0; j < i + 1; j++, k++)
+                            {
+                                builder.Insert(k, value[j]);
+                            }
+                        }
+                        else if (textType == EscapeHelpers.TextType.HtmlStart)
+                        {
+                            for (int j = indexBefore; j < i + 1; j++)
+                            {
+                                startTag.Append(value[j]);
+                            }
+
+                            insideTag = true;
+                        }
+                        else if (textType == EscapeHelpers.TextType.HtmlEnd)
+                        {
+                            for (int j = startTag.Length - 1; j > -1; j--)
+                            {
+                                current.Insert(0, startTag[j]);
+                            }
+
+                            startTag.Clear();
+
+                            // Add the skipped range
+                            for (int j = indexBefore; j < i + 1; j++)
+                            {
+                                current.Append(value[j]);
+                            }
+
+                            if (current.Length > 0)
+                            {
+                                for (int j = current.Length - 1; j > -1; j--)
+                                {
+                                    builder.Insert(0, current[j]);
+                                }
+
+                                current.Clear();
+                            }
+
+                            insideTag = false;
                         }
                     }
                 }
