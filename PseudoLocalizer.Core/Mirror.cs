c0ce230b562afb919aeb515937785f5aea4af648
martincostello <martin@martincostello.com>
2020-09-12 16:30:32 +0100

    Split Transform() in two
    Split into a fast and slow path as separate methods.

--- PseudoLocalizer.Core/Mirror.cs
+++ PseudoLocalizer.Core/Mirror.cs
@@ -16,124 +16,137 @@
         /// <inheritdoc />
         public string Transform(string value)
         {
-            var result = new StringBuilder(value.Length);
-
             // Slower path to not break formatting strings by flipping placeholders or breaking HTML
             if (EscapeHelpers.MayNeedEscaping(value))
             {
-                char[] src = value.ToArray();
+                return MirrorSlow(value);
+            }
+            else
+            {
+                return MirrorFast(value);
+            }
+        }
 
-                var current = new StringBuilder(value.Length);
-                var startTag = new StringBuilder();
+        private static string MirrorFast(string value)
+        {
+            var result = new StringBuilder(value.Length);
 
-                bool isInsideTag = false;
+            for (int i = 0; i < value.Length; i++)
+            {
+                result.Insert(0, FlipIfSpecial(value[i]));
+            }
 
-                for (int i = 0; i < value.Length; i++)
+            return result.ToString();
+        }
+
+        private static string MirrorSlow(string value)
+        {
+            char[] src = value.ToArray();
+
+            var result = new StringBuilder(value.Length);
+            var current = new StringBuilder(value.Length);
+            var startTag = new StringBuilder();
+
+            bool isInsideTag = false;
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                char ch = value[i];
+                int indexBefore = i;
+
+                if (EscapeHelpers.ShouldTransform(src, ch, ref i, out var textType))
                 {
-                    char ch = value[i];
-                    int indexBefore = i;
-
-                    if (EscapeHelpers.ShouldTransform(src, ch, ref i, out var textType))
+                    if (isInsideTag)
                     {
-                        if (isInsideTag)
-                        {
-                            // Build the reversed inner text of the HTML tag
-                            current.Insert(0, FlipIfSpecial(ch));
-                        }
-                        else
-                        {
-                            result.Insert(0, FlipIfSpecial(ch));
-                        }
+                        // Build the reversed inner text of the HTML tag
+                        current.Insert(0, FlipIfSpecial(ch));
                     }
                     else
                     {
-                        // Transformation should be skipped due to formatting placeholder or HTML
-                        if (textType == EscapeHelpers.TextType.Format)
+                        result.Insert(0, FlipIfSpecial(ch));
+                    }
+                }
+                else
+                {
+                    // Transformation should be skipped due to formatting placeholder or HTML
+                    if (textType == EscapeHelpers.TextType.Format)
+                    {
+                        if (isInsideTag)
                         {
-                            if (isInsideTag)
-                            {
-                                // Add the escaped string to the start of the
-                                // inner text for the tag in left-to-right order
-                                for (int j = indexBefore, k = 0; j < i + 1; j++, k++)
-                                {
-                                    current.Insert(k, value[j]);
-                                }
-                            }
-                            else
-                            {
-                                // Add the format string to the end in left-to-right order
-                                for (int j = indexBefore; j < i + 1; j++)
-                                {
-                                    current.Append(value[j]);
-                                }
-
-                                // Flush to the left-hand-side of the result
-                                if (current.Length > 0)
-                                {
-                                    result.Insert(0, current.ToString());
-                                    current.Clear();
-                                }
-                            }
-                        }
-                        else if (textType == EscapeHelpers.TextType.HtmlSelfClosing)
-                        {
-                            // Add the escaped string to the start
-                            // of the result in left-to-right order
+                            // Add the escaped string to the start of the
+                            // inner text for the tag in left-to-right order
                             for (int j = indexBefore, k = 0; j < i + 1; j++, k++)
                             {
-                                result.Insert(k, value[j]);
+                                current.Insert(k, value[j]);
                             }
                         }
-                        else if (textType == EscapeHelpers.TextType.HtmlStart)
+                        else
                         {
-                            // Store the start tag for use once it's closed
-                            isInsideTag = true;
-
-                            for (int j = indexBefore; j < i + 1; j++)
-                            {
-                                startTag.Append(value[j]);
-                            }
-                        }
-                        else if (textType == EscapeHelpers.TextType.HtmlEnd)
-                        {
-                            // Add the tag start as a prefix to the inner text
-                            current.Insert(0, startTag.ToString());
-                            startTag.Clear();
-
-                            // Add the tag end as a suffix to the inner text
+                            // Add the format string to the end in left-to-right order
                             for (int j = indexBefore; j < i + 1; j++)
                             {
                                 current.Append(value[j]);
                             }
 
-                            // Flush the tag to the result
+                            // Flush to the left-hand-side of the result
                             if (current.Length > 0)
                             {
-                                for (int j = current.Length - 1; j > -1; j--)
-                                {
-                                    result.Insert(0, current[j]);
-                                }
-
+                                result.Insert(0, current.ToString());
                                 current.Clear();
                             }
-
-                            isInsideTag = false;
                         }
                     }
-                }
+                    else if (textType == EscapeHelpers.TextType.HtmlSelfClosing)
+                    {
+                        // Add the escaped string to the start
+                        // of the result in left-to-right order
+                        for (int j = indexBefore, k = 0; j < i + 1; j++, k++)
+                        {
+                            result.Insert(k, value[j]);
+                        }
+                    }
+                    else if (textType == EscapeHelpers.TextType.HtmlStart)
+                    {
+                        // Store the start tag for use once it's closed
+                        isInsideTag = true;
 
-                // Flush any remaining characters
-                if (current.Length > 0)
-                {
-                    result.Insert(0, current.ToString());
+                        for (int j = indexBefore; j < i + 1; j++)
+                        {
+                            startTag.Append(value[j]);
+                        }
+                    }
+                    else if (textType == EscapeHelpers.TextType.HtmlEnd)
+                    {
+                        // Add the tag start as a prefix to the inner text
+                        current.Insert(0, startTag.ToString());
+                        startTag.Clear();
+
+                        // Add the tag end as a suffix to the inner text
+                        for (int j = indexBefore; j < i + 1; j++)
+                        {
+                            current.Append(value[j]);
+                        }
+
+                        // Flush the tag to the result
+                        if (current.Length > 0)
+                        {
+                            for (int j = current.Length - 1; j > -1; j--)
+                            {
+                                result.Insert(0, current[j]);
+                            }
+
+                            current.Clear();
+                        }
+
+                        isInsideTag = false;
+                    }
                 }
             }
-            else
+
+            // Flush any remaining characters
+            if (current.Length > 0)
             {
-                foreach (char ch in value.Reverse())
-                {
-                    result.Append(FlipIfSpecial(ch));
-                }
+                result.Insert(0, current.ToString());
             }
 
             return result.ToString();
